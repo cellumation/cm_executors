abcb25cb06f56f40e5d33b4687e3481057273b30
Janosch Machowinski <J.Machowinski@cellumation.com>
2026-02-10 19:06:57 +0100

    test: Fix data race

--- test/executors/test_executors.cpp
+++ test/executors/test_executors.cpp
@@ -82,7 +82,7 @@ public:
   rclcpp::Node::SharedPtr node;
   rclcpp::Publisher<test_msgs::msg::Empty>::SharedPtr publisher;
   rclcpp::Subscription<test_msgs::msg::Empty>::SharedPtr subscription;
-  int callback_count;
+  std::atomic<int> callback_count;
 };
 
 template<typename T>
@@ -311,7 +311,7 @@ TYPED_TEST(TestExecutors, testSpinUntilFutureCompleteWithTimeout)
   ExecutorType executor;
   executor.add_node(this->node);
 
-  bool spin_exited = false;
+  std::atomic<bool> spin_exited = false;
 
   // Needs to run longer than spin_until_future_complete's timeout.
   std::future<void> future = std::async(
@@ -703,7 +703,7 @@ TYPED_TEST(TestExecutors, testSpinUntilFutureCompleteInterrupted)
   ExecutorType executor;
   executor.add_node(this->node);
 
-  bool spin_exited = false;
+  std::atomic<bool> spin_exited = false;
 
   // This needs to block longer than it takes to get to the shutdown call below and for
   // spin_until_future_complete to return
@@ -768,7 +768,7 @@ TYPED_TEST(TestExecutors, testService)
 
   using Service = test_msgs::srv::Empty;
 
-  bool gotCallback = false;
+  std::atomic<bool> gotCallback = false;
 
   auto service_cb = [&gotCallback](const std::shared_ptr<Service::Request>/*request*/,
     std::shared_ptr<Service::Response>/*response*/)
@@ -845,7 +845,7 @@ TYPED_TEST(TestExecutors, addAfterSpin)
 
   using Service = test_msgs::srv::Empty;
 
-  bool gotCallback = false;
+  std::atomic<bool> gotCallback = false;
 
   auto service_cb = [&gotCallback](const std::shared_ptr<Service::Request>/*request*/,
     std::shared_ptr<Service::Response>/*response*/)
