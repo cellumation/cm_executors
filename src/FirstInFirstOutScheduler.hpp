e0f87a7b000cf2cda76a256f4e4f1e18b85a7976
Janosch Machowinski <J.Machowinski@cellumation.com>
2026-02-13 15:33:03 +0100

    feat: Only wake worker threads if needed
    This commit fixes the 'tramping horde' effect of the worker
    threads all trying to get work. From now on, a worker thread
    is only woken up, if there is work that can be executed in
    a parallel way.

--- src/FirstInFirstOutScheduler.hpp
+++ src/FirstInFirstOutScheduler.hpp
@@ -63,8 +63,8 @@ private:
 class FirstInFirstOutScheduler : public CBGScheduler
 {
 public:
-  std::optional<ExecutableEntity> get_next_ready_entity() final;
-  std::optional<ExecutableEntity> get_next_ready_entity(
+  ExecutableEntityWithInfo get_next_ready_entity() final;
+  ExecutableEntityWithInfo get_next_ready_entity(
     GlobalEventIdProvider::MonotonicId max_id) final;
 
 private:
